b2467a13c2f22451631b2c2e79a84b2ffad55d4e
mahmoud ali <49574153+mahmoudali01@users.noreply.github.com>
2022-06-08 04:17:04 +0200

    test login form with different data

--- src/test/java/loginTest.java
+++ src/test/java/loginTest.java
@@ -12,7 +12,10 @@ public class loginTest {
         SoftAssert soft = new SoftAssert();
     loginPage login;
 
-//    public void loginSteps(String username , String password){
+//    public loginTest() throws Exception {
+//    }
+
+    //    public void loginSteps(String username , String password){
 //        login.usernameElementPOM(driver).clear();
 //        login.usernameElementPOM(driver).sendKeys(username);
 //        login.passElementPOM(driver).sendKeys(password);
@@ -32,9 +35,13 @@ public class loginTest {
         }
 
         @Test
-        void vaildData() throws InterruptedException {
+        void test() throws Exception {
+            String [][]testData= Util.getDataFromExcel(Util.FILE_PATH,Util.SHEET_NAME,Util.TABLE_NAME);
 
-     login.loginSteps( "valid", "valid");
+            for (int i = 0 ; i< testData.length;i++){
+          login.loginSteps(testData[i][0], testData[i][1]);}
+
+
 
          //   String er ="You logged into a secure area!";
 
@@ -45,23 +52,23 @@ public class loginTest {
            // soft.assertAll();
             Thread.sleep(3000);
         }
-        @Test
-        void invaildData() throws InterruptedException {
-            login.loginSteps( "invalid", "invalid!");
-
-//            driver.findElement(By.id("username")).clear();
+//        @Test
+//        void invaildData() throws InterruptedException {
+//            login.loginSteps( "invalid", "invalid!");
 //
-//            driver.findElement(By.id("username")).sendKeys("wrong");
-//            driver.findElement(By.id("password")).sendKeys("wrong!");
-//            driver.findElement(By.id("password")).sendKeys(Keys.ENTER);
-//            String er ="Your username is invalid!";
-//            String ar =  login.flashPOM().getText();
-//            soft.assertEquals(ar.contains(er),true);
-//            soft.assertTrue(ar.contains(er));
-//            soft.assertAll();
-
-            Thread.sleep(3000);
-        }
+////            driver.findElement(By.id("username")).clear();
+////
+////            driver.findElement(By.id("username")).sendKeys("wrong");
+////            driver.findElement(By.id("password")).sendKeys("wrong!");
+////            driver.findElement(By.id("password")).sendKeys(Keys.ENTER);
+////            String er ="Your username is invalid!";
+////            String ar =  login.flashPOM().getText();
+////            soft.assertEquals(ar.contains(er),true);
+////            soft.assertTrue(ar.contains(er));
+////            soft.assertAll();
+//
+//            Thread.sleep(3000);
+//        }
         @AfterTest
         void closeDriver(){
             driver.quit();
